eac00aa509c0955e924ed66028b00341d8ff3918
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-12-05 15:34:57 +0200

    up

--- exam/src/main/java/prep/web/HomeController.java
+++ exam/src/main/java/prep/web/HomeController.java
@@ -1,13 +1,15 @@
 package prep.web;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String index(){
-        return "index";
+    public String index(HttpSession httpSession){
+        return httpSession.getAttribute("user")==null ?
+                "index" : "home";
     }
 
 }
